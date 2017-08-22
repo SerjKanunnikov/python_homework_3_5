be82029aeecdbb0ea0e1ad74062e2ab780b6e5c1
Kanunnikov Sergey <SergeyKanunnikov@MacBook-Pro-Kanunnikov.local>
2017-08-22 11:02:02 +0300

    distance conversion

--- python_homework_3_5.py
+++ python_homework_3_5.py
@@ -21,14 +21,39 @@ def currency(currencies):
             destination = trip.split(" ")[0]
             cost = trip.split(" ")[1]
             curr = trip.split(" ")[2].strip()
-            print(destination, cl.service.ConvertToNum(
+            print(destination, round(cl.service.ConvertToNum(
                 fromCurrency=curr,
                 toCurrency="RUB",
                 rounding=False,
                 amount=cost
-            ))
+            )), "RUB")
+
+
+def convert_distance(dist):
+    cl = osa.Client("http://www.webservicex.net/length.asmx?WSDL")
+    total_distance = 0
+    with open(dist, encoding="utf-8") as f:
+        for trip in f:
+            destination = trip.split(" ")[0]
+            distance = trip.split(" ")[1]
+            measure = trip.split(" ")[2].strip()
+            print(destination)
+            print(type(destination))
+            print(distance)
+            print(type(distance))
+            print(measure)
+            print(type(measure))
+            trip_distance = cl.service.ChangeLengthUnit(
+                LengthValue=str(distance),
+                fromLengthUnit="Miles",
+                toLengthUnit="Kilometers",
+            )
+            print(trip_distance)
+            total_distance += trip_distance
+    print(total_distance)
 
 
 if __name__ == "__main__":
     # convert_temp("temps.txt")
-    currency("currencies.txt")
+    # currency("currencies.txt")
+    convert_distance("travel.txt")
